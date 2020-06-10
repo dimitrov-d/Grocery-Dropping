d18c93020476e50a8cabb3e1ceb025d566210019
Damjan <damjandimitrov1@gmail.com>
2020-06-10 11:23:29 +0200

    Update navbar routerlinks

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/auth.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class NavbarComponent {
+  currentUser: User;
+  constructor(private authService: AuthenticationService) {
+    this.currentUser = this.authService.currentUserSubject.value || null;
   }
 
+  logout() {
+    location.reload();
+    setTimeout(() => {
+      this.currentUser = null;
+    }, 1000);
+    this.authService.logout();
+  }
 }
