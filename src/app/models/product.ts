6231a313590ea5c927a87c8202424dfdb0512271
Damjan <damjandimitrov1@gmail.com>
2020-06-15 20:35:59 +0200

    Added product/s model and test items

---
+++ src/app/models/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+    title: string;
+    category: string;
+    price: number;
+    supermarket: string;
+    imageUrl: string;
+}
---
+++ src/assets/products.ts
@@ -0,0 +1,60 @@
+import { Product } from 'src/app/models/product';
+
+export const products: Product[] = [
+  {
+    title: 'bread',
+    category: 'bread',
+    price: 0.8,
+    supermarket: 'Lidl',
+    imageUrl:
+      'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+  {
+    title: 'bread',
+    category: 'bread',
+    price: 0.8,
+    supermarket: 'Lidl',
+    imageUrl:
+      'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+  {
+    title: 'bread',
+    category: 'bread',
+    price: 0.8,
+    supermarket: 'Lidl',
+    imageUrl:
+      'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+  {
+    title: 'bread',
+    category: 'bread',
+    price: 0.8,
+    supermarket: 'Lidl',
+    imageUrl:
+      'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+  {
+    title: 'bread',
+    category: 'bread',
+    price: 0.8,
+    supermarket: 'Lidl',
+    imageUrl:
+      'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+  {
+    title: 'bread',
+    category: 'bread',
+    price: 0.8,
+    supermarket: 'Lidl',
+    imageUrl:
+      'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+  {
+    title: 'bread',
+    category: 'bread',
+    price: 0.8,
+    supermarket: 'Lidl',
+    imageUrl:
+      'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+];
