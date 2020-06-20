46a273814361757bb63419b1d33203f69e1e4593
Damjan <damjandimitrov1@gmail.com>
2020-06-20 22:30:42 +0200

    Implement custom cart component (WIP)

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent implements OnInit {
+export class CartComponent {
+  cart_items: Observable<any[]>;
+  total: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private db: AngularFirestore) {
+    this.cart_items = this.getItems();
+    db.collection('/cart')
+      .valueChanges()
+      .subscribe((result) => {
+        console.log(result.length);
+        this.total = result.length;
+      });
   }
 
+  getItems() {
+    return this.db.collection('/cart').valueChanges();
+  }
+
+  getUrl() {
+    return "url('https://images.pexels.com/photos/5617/red-tomato-vegetable.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940') no-repeat center center fixed;";
+  }
 }
