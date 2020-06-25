0408dcfedd3f2ddbffdd81e65a9f6dc586f9eaee
Damjan <damjandimitrov1@gmail.com>
2020-06-25 22:37:31 +0200

    Implement modal component to choose delivery option

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent implements OnInit {
+  pickup = false;
+  text = 'Please choose your desired option of order receival';
+  myDate: any;
+  constructor(private cart: CartService) {}
+
+  ngOnInit() {}
+
+  setPickup() {
+    this.pickup = true;
+    this.text =
+      'Please choose when you would like your products to be ready for pickup';
+  }
+
+  done() {
+    this.cart.completeOrder();
+  }
+}
