d7effc4500cd568d012897a593a8597a2e86f3ae
Damjan <damjandimitrov1@gmail.com>
2020-06-26 13:52:51 +0200

    Cart Icon total number of prods subicon added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/auth.service';
 import { User } from 'src/app/models/user';
 import { CartService } from 'src/app/services/cart.service';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-navbar',
@@ -10,11 +11,18 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class NavbarComponent {
   currentUser: User;
+  cartTotal: number;
   constructor(
     private authService: AuthenticationService,
-    private cart: CartService
+    private cart: CartService,
+    private db: AngularFirestore
   ) {
     this.currentUser = this.authService.currentUserSubject.value || null;
+    db.collection('/cart')
+      .valueChanges()
+      .subscribe((products) => {
+        this.cartTotal = products.length;
+      });
   }
 
   logout() {
