6b59473e3d129a7cefcefa2751e8a35f48fcf9f5
Damjan <damjandimitrov1@gmail.com>
2020-06-28 11:42:15 +0200

    Finish saved orders logic and CRUD

--- src/app/components/saved/saved.component.ts
+++ src/app/components/saved/saved.component.ts
@@ -1,19 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-saved',
   templateUrl: './saved.component.html',
   styleUrls: ['./saved.component.css'],
 })
-export class SavedComponent implements OnInit {
+export class SavedComponent {
+  saved_items: Observable<any[]>;
+  savedTotal: number;
 
-  saved_items: any;
-  
   constructor(private db: AngularFirestore) {
     this.saved_items = this.db.collection('/saved').valueChanges();
-    this.saved_items.subscribe((x) => console.log(x));
+    this.saved_items.subscribe((saved) => {
+      this.savedTotal = saved.length;
+    });
   }
 
-  ngOnInit() {}
+  deleteOrder(index: number) {
+    this.db.collection('/saved').doc(index.toString()).delete();
+  }
+
+  addOrderToCart(index: number) {
+    this.db
+      .collection('/saved')
+      .doc(index.toString())
+      .valueChanges()
+      .subscribe((x: any) => {
+        let products: any[] = x.prods;
+        products.forEach((prod) => this.addToCart(prod));
+      });
+  }
+
+  addToCart(product) {
+    this.db.collection('/cart').doc(product.id.toString()).set(product);
+  }
+
+  clearOrders() {
+    this.db
+      .collection('/saved')
+      .get()
+      .toPromise()
+      .then((res) => {
+        res.forEach((order) => {
+          order.ref.delete();
+        });
+      });
+  }
 }
